f350e6d26ed331e68bdd4cc8feb0256308c3462b
Aleksandr Ulshin <aleksandr.ulshin@rhsolutions.local>
2023-03-22 12:15:53 +0300

    '111'

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -10,6 +10,7 @@ use App\Http\Requests\TaskStoreRequest;
 use App\Http\Requests\TaskUpdateRequest;
 use App\Models\Label;
 use Spatie\QueryBuilder\QueryBuilder;
+use Spatie\QueryBuilder\AllowedFilter;
 
 class TaskController extends Controller
 {
@@ -29,7 +30,11 @@ class TaskController extends Controller
         $users = User::all()->pluck("name", "id");
 
         $tasks = QueryBuilder::for(Task::class)
-            ->allowedFilters(["status_id", "created_by_id", "assigned_to_id"])
+            ->allowedFilters([
+                AllowedFilter::exact('status_id'),
+                AllowedFilter::exact('created_by_id'),
+                AllowedFilter::exact('assigned_to_id'),
+            ])
             ->orderBy('id')
             ->paginate(10);
 
